f78db39defa64ea9fd383ac25494a25b7c969f91
Mohammed Nihal <mohammednihal691@gmail.com>
2024-03-23 19:11:31 +0530

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,14 +24,15 @@ const Header = () => {
           </div>
         </div>
         <div
-          className=" rounded-full p-0.5"
+          className=" rounded-full p-0.5  overflow-clip"
           style={{ background: " rgba(209, 117, 182, 1)" }}
         >
+          
           <img
             className="rounded-full"
             width={50}
-            src="https://s3-alpha-sig.figma.com/img/2efb/d882/6436e7705a635d52495da5ff20598036?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N-6FoKpAmZzERaYyrfFqoKdl5KWjpsQSx9ytiV6NRjq3D2Cx02Az-g86bZUxVXSk3zjmpCwSwT7128Idcx-CUUr~7F1gKR4j4t00kmiZ39oXY1yIU7mAzci2nK~yo7uaeYPVpOmogR-dCAMPFDVtQ88vbSxOcPoPNFy7DPIlTZarfXu9xIbQDs~-orbE~~f4xdz6ji7x5AkVPqwVKNiCZ-1~zQRbcrbH6GEw-4SyzrIQVdRlSnSWQgp3jfag2yVCxAsqXWn1ya8K0wmCsrxdwbtHwq18CQjytCpG9sc6Sd0Uk1kieuk3ULBH1fwg4AX~ewr0xfDq1b8hah~8EO-qGA__"
-            alt=""
+            src="./img-1.jpg"
+            alt="loading"
           />
         </div>
       </div>
